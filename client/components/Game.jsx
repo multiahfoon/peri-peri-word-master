784b00f0b581400222b46c47ff15344fe295bd0b
Pete-Angjelevski <73816283+Pete-Angjelevski@users.noreply.github.com>
2021-02-05 10:22:23 +1300

    add pseudocode

--- client/components/Game.jsx
+++ client/components/Game.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function Game () {
 
-  
+
   return (
 
   )
 }
+
+
+/* create function that runs list of words from array to be passed into API function and provide us with word synonym object. have user input compare ther word against
+word in synonym field of word object, if correct load correct +1 to counter - if incorrect load incorrect and no counter. 
+form field to be within return of current function */ 
