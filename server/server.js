8e5034af8cf9fd6cc7617f1a5f7065bf5e93c1b2
Peter <peter.lim91@gmail.com>
2021-02-04 17:38:20 +1300

    tidy up template

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const path = require('path')
 const express = require('express')
 
-const learnerRoutes = require('./routes')
+const routes = require('./routes')
 
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
 
-server.use('/api/v1/learners', learnerRoutes)
+server.use('/api/v1/words', routes)
 
 module.exports = server
