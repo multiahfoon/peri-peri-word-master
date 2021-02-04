13bad151236088411fd52cff3984e61d429d7c41
Multi Ah Foon <35914987+multiahfoon@users.noreply.github.com>
2021-02-05 10:13:33 +1300

    add api to server

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,24 @@
+// const path = require('path')
 const express = require('express')
+const request = require('superagent')
 
-const router = express.Router()
+const server = express()
+const randomWord = 'lovely'
+const apiUrl = `https://wordsapiv1.p.rapidapi.com/words/${randomWord}/synonyms`
+const apiKey = ''
 
-module.exports = router
+// Put api/v1/ routes here
+// so when this route is call do as followed
+server.get('/', (req, res) => {
+  console.log('route working')
+  request.get(apiUrl)
+    .query({ 'rapidapi-key': apiKey })
+    .then(apiRes => {
+      const result = apiRes.body
+      console.log(apiRes.body)
+      return res.json(result)
+    })
+    .catch(err => console.log(err))
+})
 
-//Put api/v1/ routes here
+module.exports = server
